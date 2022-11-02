0f805d2a8a8cdfb812dd61dfdaf50e5660d1e26e
jaderAdriel1 <jaderadriel7@gmail.com>
2022-11-02 15:41:52 -0300

    add feature of get route by user location and adding style

--- assents/js/index.js
+++ assents/js/index.js
@@ -2,8 +2,8 @@
 import { Map } from './abstract/Map.js';
 
 function initMap() {
-
-    const guanambi = { lat: -14.283216, lng: -42.683485 };
+    
+    const guanambi = { lat: -14.277228, lng: -42.702106 };
 
     const map = new Map({
       zoom: 10, 
@@ -12,24 +12,83 @@ function initMap() {
       element: document.getElementById("map")
     })
 
-
-    const form = document.getElementById('form');
-    form.addEventListener('submit', (e) => {
-
-        e.preventDefault()
-        let position =  getSelectedChoise(form);
-
-        map.changeMarkerPosition(position);
-        map.createRoute(guanambi, position)
-        .then(() => setRouteInformations(map.distanceRoute, map.durationRoute) )
-        
-    });
-
+    main(map, guanambi)
+    
 }
 window.initMap = initMap;
 
 
+function main(map, startPositionn) {
+    const form = document.getElementById('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        traceRoute(map, startPositionn, getSelectedChoise(form))
+    });
+    document.getElementById('buttonClick').addEventListener('click', (e) => {
+        if (!navigator.geolocation) {
+            alert('Erro! não da pra localizar vocẽ')
+            return
+        }
+        
+        navigator.geolocation.getCurrentPosition((position) => {
+            let location = {lat : position.coords.latitude, lng: position.coords.longitude}
+            traceRoute(map, startPositionn, location)
+        },handle_errors);
+    })
+}
 
+
+function handle_errors(error){
+    switch(error.code)
+    {
+        case error.PERMISSION_DENIED: alert(
+            "ative a geolocalização, se estiver no chrome vá em 'chrome://settings/content/location'"
+            );
+        break;
+
+        case error.POSITION_UNAVAILABLE: alert("could not detect current position");
+        break;
+
+        case error.TIMEOUT: alert("retrieving position timed out");
+        break;
+
+        default: alert("unknown error");
+        break;
+    }
+}
+
+
+function traceRoute(map, startPosition, endPosition) {
+    let response = map.createRoute(startPosition, endPosition);
+
+    startLoading()
+
+    response.finally(() => { endLoading()})
+        .then(
+            () => {
+                changeStyle('hsla(230, 69%, 50%, 0.8)', 'info')
+                setRouteInformations(map.distanceRoute, map.durationRoute);
+            },
+            (error) => {
+                changeStyle('red', 'info')
+                setRouteInformations(error.message, error.message);
+            }
+        )
+    setTimeout(() => {
+        changeStyle('rgba(71,71,71,0.2)', 'info')
+    }, 5000)
+}
+
+
+function changeStyle(color,classe) {
+    let elements = Array.from( document.querySelectorAll(`.${classe}`) )
+    
+    elements.forEach((element) => {
+        console.log(element.style.boxShadow)
+        element.style.boxShadow = `0px 0px 3px 1px ${color}`;
+    })
+
+}
 
 
 function getSelectedChoise(form) {
@@ -40,6 +99,23 @@ function getSelectedChoise(form) {
     }
 }
 
+
+function startLoading() {
+    let loader = document.getElementById("loader");
+    loader.style.animationPlayState = 'running';
+    loader.style.display = "block";
+    document.getElementById("loader-wrapper").style.display = "block";
+}
+
+
+function endLoading() {
+    let loader = document.getElementById("loader");
+    loader.style.animationPlayState = 'paused';
+    loader.style.display = "none";
+    document.getElementById("loader-wrapper").style.display = "none";
+}
+
+
 function getPosition(value){
     value = value.split(",")
     for(let i =0; i < value.length; i++) {
@@ -47,6 +123,7 @@ function getPosition(value){
     }
     return { lat: value[0], lng: value[1] }
 }
+
 
 function setRouteInformations(distance, duration) {
     let distanceElement = document.getElementById('distanceRouteMap');
